624bda41118dcadc84f886953645131579464eaf
Maik Hilker <maik@hilker.io>
2019-10-25 18:48:23 +0200

    added error handling for all errors in Index.js

--- src/Index.js
+++ src/Index.js
@@ -1,34 +1,39 @@
 const ice = require('./Ice');
 
 async function Index(req, res) {
-    const serverId = 1;
+    try {
+        const serverId = 1;
 
-    const users = await ice.getUsers(serverId);
+        const users = await ice.getUsers(serverId);
 
-    const channels = await ice.getChannels(serverId);
-    const channelsWithUsers = channels.map(c => {
-        c.users = users.filter(u => u.channel === c.id).length;
-        return c;
-    });
+        const channels = await ice.getChannels(serverId);
+        const channelsWithUsers = channels.map(c => {
+            c.users = users.filter(u => u.channel === c.id).length;
+            return c;
+        });
 
-    const channelsById = channelsWithUsers.reduce((channels, c) => {
-        channels[c.id] = c;
-        return channels;
-    }, {});
+        const channelsById = channelsWithUsers.reduce((channels, c) => {
+            channels[c.id] = c;
+            return channels;
+        }, {});
 
-    const tree = channelsWithUsers.reduce((tree, c) => {
-        if (!tree[c.parent]) {
-            tree[c.parent] = []
-        }
+        const tree = channelsWithUsers.reduce((tree, c) => {
+            if (!tree[c.parent]) {
+                tree[c.parent] = []
+            }
 
-        tree[c.parent].push(c.id);
-        return tree;
-    }, {});
+            tree[c.parent].push(c.id);
+            return tree;
+        }, {});
 
-    res.render('index', {
-        channels: channelsById,
-        tree: tree,
-    });
+        res.render('index', {
+            channels: channelsById,
+            tree: tree,
+        });
+    } catch (e) {
+        console.log(e);
+        res.send(500);
+    }
 }
 
 module.exports = Index;
